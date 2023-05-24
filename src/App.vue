257851035b0ff9861865674110eeebff5d9b4167
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-05-24 22:43:52 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+function startObserver(){
+  
+}
+</script>
+
+<template>
+  <body>
+    <div>
+      <button @click="">startchecking</button>
+    </div>
+  </body>
+</template>
+
+<style scoped></style>
