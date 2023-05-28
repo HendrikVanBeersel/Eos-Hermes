133b1490a321ce627dfd7b2869d7f40a6bc85e59
Henry <Hendrikvanbeersel@hotmail.com>
2023-05-29 00:39:18 +0200

    completly revised the template

---
+++ popup/popup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Click me!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, world!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Add your custom styles or Tailwind classes here */
+</style>
